165a2409f22e50813e2c21d6f87be16d02fc02ae
Erik Wiberg <erik.wiberg.87@gmail.com>
2016-04-20 15:07:40 +0200

    add empty spring boot frame..

---
+++ src/main/java/filmr/Application.java
@@ -0,0 +1,16 @@
+package filmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
