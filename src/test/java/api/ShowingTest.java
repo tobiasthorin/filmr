81c86f6e8413e9c7f65102d9ed11bef8e6fa7d2a
luffarvante <adrian.kinberger@gmail.com>
2016-04-25 14:06:31 +0200

    clean ShowingTest

--- src/test/java/api/ShowingTest.java
+++ src/test/java/api/ShowingTest.java
@@ -5,40 +5,61 @@ import filmr.domain.Booking;
 import filmr.domain.Movie;
 import filmr.domain.Showing;
 import filmr.domain.Theater;
-import org.junit.*;
-import org.junit.rules.ExpectedException;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
- * Created by luffarvante on 2016-04-22.
+ * Created by luffarvante on 2016-04-25.
  */
-public class ApiTest {
+@RunWith(Parameterized.class)
+public class ShowingTest {
 
     private static ConfigurableApplicationContext applicationContext;
 	private String showingApiBaseUrl = "http://localhost:8080/filmr/api/showings";
 	private RestTemplate restTemplate;
 	private Showing testShowing;
+    private Long id;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> ids() {
+        return Arrays.asList(new Object[][]{
+                {new Long(1)},
+                {new Long(2)},
+                {new Long(3)},
+                {new Long(4)},
+                {new Long(5)},
+                {new Long(6)},
+                {new Long(7)},
+                {new Long(8)},
+                {new Long(9)},
+                {new Long(10)},
+        });
+    }
+
+    public ShowingTest(Long id) {
+        this.id = id;
+    }
 
 	@BeforeClass // just do this once, for the whole test class (not for each method, like @Before)
 	public static void testClassSetup(){
-		// start application so that API is running
+		/* start application so that API is running */
 		applicationContext = SpringApplication.run(Application.class, new String[0]);
 	}
 
@@ -73,19 +94,13 @@ public class ApiTest {
 		assertEquals(s.getBookings(), postedShowing.getBookings());
 		assertEquals(s.getMovie(), postedShowing.getMovie());
 		assertEquals(s.getTheater(), postedShowing.getTheater());
-
-//		assertEquals(testShowing.getShowDateTime(), postedShowing.getShowDateTime());
-//		assertEquals(testShowing.getBookings(), postedShowing.getBookings());
-//		assertEquals(testShowing.getMovie(), postedShowing.getMovie());
-//		assertEquals(testShowing.getTheater(), postedShowing.getTheater());
 	}
 
-	@Test
+
+
+    @Test
 	public void testRead() {
 		//Checks if API returns object with supplied ID
-		//TODO proper parameter
-		Long id = new Long(1);
-
 		ResponseEntity<Showing> readShowingResponseEntity = restTemplate.getForEntity(showingApiBaseUrl+"/"+id, Showing.class);
 		Showing readShowing = readShowingResponseEntity.getBody();
 
@@ -94,13 +109,11 @@ public class ApiTest {
 
 	@Test
 	public void testUpdate() {
-		//TODO proper paramater
-		Long id = new Long(1);
-
 		ResponseEntity<Showing> showingResponseEntity = restTemplate.getForEntity(showingApiBaseUrl+"/"+id, Showing.class);
 		Showing updateToThis = showingResponseEntity.getBody();
 
 		//TODO update some values in the object here
+        //updateToThis.setMovie(new Movie());
 
 		restTemplate.put(showingApiBaseUrl+"/"+id, updateToThis);
 
@@ -119,21 +132,16 @@ public class ApiTest {
 
 	@Test(expected = HttpClientErrorException.class)
 	public void testUpdateWithoutId() {
-		//TODO proper parameter
-		Long id = new Long(1);
 		//This will send a showing to put WITHOUT ID; it should return a 400 Bad Request
 		restTemplate.put(showingApiBaseUrl + "/"+id, new Showing());
 	}
 
 	@Test(expected = HttpServerErrorException.class)
 	public void testDelete() {
-		//TODO proper parameter
-		Long id = new Long(1);
 		//Delete object
 		restTemplate.delete(showingApiBaseUrl+"/"+id, Showing.class);
-		//Try to read object at id; should not work
-		ResponseEntity<Showing> showingResponseEntity = restTemplate.getForEntity(showingApiBaseUrl + "/" + id, Showing.class);
-		Showing showing = showingResponseEntity.getBody();
+		//Try to read object at id; should not work. If object does not exist, throws a 500 HttpServerErrorException
+		restTemplate.getForEntity(showingApiBaseUrl + "/" + id, Showing.class);
 	}
 
     @Test
