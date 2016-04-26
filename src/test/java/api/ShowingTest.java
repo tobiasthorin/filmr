38a08b06d1a02be46c41d651898e629b45c4fff3
luffarvante <adrian.kinberger@gmail.com>
2016-04-26 10:42:51 +0200

    clean up showing test

--- src/test/java/api/ShowingTest.java
+++ src/test/java/api/ShowingTest.java
@@ -24,6 +24,7 @@ import java.util.Collection;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * Created by luffarvante on 2016-04-25.
