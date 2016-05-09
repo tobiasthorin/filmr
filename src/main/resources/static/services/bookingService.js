9fb0a94793650c5345dd3ee45a6158b759c576a2
emil <>
2016-05-09 09:37:39 +0200

    fix use of parameter from frontend to show distinct movies

--- src/main/resources/static/services/bookingService.js
+++ src/main/resources/static/services/bookingService.js
@@ -8,21 +8,26 @@ angular.module('filmr')
 		
 		var showingsBaseUrl = $rootScope.API_baseUrl + "showings/";
 		
-		this.getAllRelevantShowings = function(fromDate, toDate, mininumAvailableTickets, onlyForMovieWithId) {
+		this.getAllRelevantShowings = function(fromDate, toDate, mininumAvailableTickets, onlyForMovieWithId,include_distinct_movies_in_header) {
 			
-			// set default values if all values where not provided
+			// set default values if all values where not provided. TODO: use typeof undefined
 			var fromDate = fromDate ? fromDate : "";
 			var toDate = toDate ? toDate : "";
+            var include_distinct_movies_in_header = include_distinct_movies_in_header ? include_distinct_movies_in_header : false;
 			var mininumAvailableTickets = mininumAvailableTickets || 2;
 			var onlyForMovieWithId = onlyForMovieWithId ? onlyForMovieWithId : ""; //TODO: this is kind of hack, we should fix so paramters is not send if not set instead of empty string
 			
-			return $http({ 
-				"url": showingsBaseUrl,
-				"params": {
+
+            var params = {
 					"only_for_movie_with_id":onlyForMovieWithId,
 					"from_date":fromDate,
-					"to_date":fromDate
-				}
+					"to_date":fromDate,
+                    "include_distinct_movies_in_header":include_distinct_movies_in_header
+			};
+
+			return $http({ 
+				"url": showingsBaseUrl,
+				"params": params
 			}
 			); 
 		};
