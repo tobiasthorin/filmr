d27fa46068066556fbf333726ae618d2868df8c8
Erik Wiberg <erik.wiberg.87@gmail.com>
2016-05-13 15:26:51 +0200

    comment out exception handling controller

--- src/main/java/filmr/errorhandling/RestResponseEntityExceptionHandler.java
+++ src/main/java/filmr/errorhandling/RestResponseEntityExceptionHandler.java
@@ -1,31 +1,31 @@
-package filmr.errorhandling;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-//TODO: figure out why this doesn't get called
-@ControllerAdvice
-@EnableWebMvc
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-	
-    @ExceptionHandler(value = {IllegalEntityPropertyException.class})
-    public ResponseEntity<Object> handleConflict(RuntimeException exception, WebRequest request) {
-    	
-        // create a body for our response entity,
-    	System.out.println("in RestResponseEntityExceptionHandler. runtime e is: " + exception);
-    	
-    	String bodyOfResponse = 
-        		"Error thrown from class " + exception.getClass().getName() + 
-        		" with message '" + exception.getMessage() + "'";
-        return handleExceptionInternal(exception, bodyOfResponse, 
-          new HttpHeaders(), HttpStatus.CONFLICT, request);
-    }
-	
-	
-}
+//package filmr.errorhandling;
+//
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.context.request.WebRequest;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+//
+////TODO: figure out why this doesn't get called
+//@ControllerAdvice
+//@EnableWebMvc
+//public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+//	
+//    @ExceptionHandler(value = {IllegalEntityPropertyException.class})
+//    public ResponseEntity<Object> handleConflict(RuntimeException exception, WebRequest request) {
+//    	
+//        // create a body for our response entity,
+//    	System.out.println("in RestResponseEntityExceptionHandler. runtime e is: " + exception);
+//    	
+//    	String bodyOfResponse = 
+//        		"Error thrown from class " + exception.getClass().getName() + 
+//        		" with message '" + exception.getMessage() + "'";
+//        return handleExceptionInternal(exception, bodyOfResponse, 
+//          new HttpHeaders(), HttpStatus.CONFLICT, request);
+//    }
+//	
+//	
+//}
