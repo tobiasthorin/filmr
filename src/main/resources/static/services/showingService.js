d04e63437bcd2410b6ea367c9fa7ab1e509d4db4
Erik Wiberg <erik.wiberg.87@gmail.com>
2016-05-20 00:06:55 +0200

    add simple demo of how to change code to fit with /schedule

--- src/main/resources/static/services/showingService.js
+++ src/main/resources/static/services/showingService.js
@@ -2,9 +2,11 @@
  * Crus operations for Showings
  */
 angular.module('filmr').factory('ShowingService', function($resource, $rootScope){
-    var showingBaseUrl = $rootScope.API_baseUrl + "showings/";
+    var showingBaseUrl = $rootScope.API_baseUrl + "showings/schedule";
     return $resource(showingBaseUrl+":id", { id: '@id' }, {
-        update: {method: 'PUT'}
+        update: {method: 'PUT'},
+        query: {method: 'GET', isArray: false}
+        
     });
     //End of service
 });
