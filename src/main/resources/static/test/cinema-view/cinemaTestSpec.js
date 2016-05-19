7e44e731c0e04af77d6048c317928d4fdfe187cd
emil <>
2016-05-19 11:29:07 +0200

    CINEMA FRONTEND: fix test and refactor contollerC

--- src/main/resources/static/test/cinema-view/cinemaTestSpec.js
+++ src/main/resources/static/test/cinema-view/cinemaTestSpec.js
@@ -1,242 +1,249 @@
 describe("cinemaController.js", function () {
 
-	//Specify module
-	beforeEach(module('filmr'));
+    //Specify module
+    beforeEach(module('filmr'));
 
-	// Mocked data
-	var mockedSavedTheater = {};
-	var mockedMoviesInRepertoire = [
-		{name: "Lion king"},
-		{name: "Lion queen"},
-		{name: "Alien"}
-	];
-	var mockedRepertoire = {
-		movies: mockedMoviesInRepertoire
-	};
-	var mockedAddableMovies = [
-		{name: "Mars Attacks"},
-		{name: "Deer Hunter"}
-	];
-	var mockedTheaters = [
-		{name: "sal01"},
-		{name: "sal02"}
-	];
-	var mockedCinema = {id: 1, name: "Lasses Biograf", repertoire: {id: 567}};
-	var mockedRepetoireUpdateSuccessResult = {movies: [{name: 'Lion king'}, {name: 'Lion queen'}, {name: 'Alien'}, {name: 'Mars Attacks'}]};
-    var mockedAddableMoviesAfterRepertoireUpdate =  {movies: [{name: 'Lion queen'}, {name: 'Alien'}, {name: 'Mars Attacks'}]};
+    // Mocked data
+    var mockedSavedTheater = null;
+    var mockedMoviesInRepertoire = [
+        {name: "Lion king"},
+        {name: "Lion queen"},
+        {name: "Alien"}
+    ];
+    var mockedRepertoire = {
+        movies: mockedMoviesInRepertoire
+    };
+    var mockedAddableMovies = [
+        {name: "Mars Attacks"},
+        {name: "Deer Hunter"}
+    ];
+    var mockedTheaters = [
+        {name: "sal01"},
+        {name: "sal02"}
+    ];
+    var mockedCinema = {id: 1, name: "Lasses Biograf", repertoire: {id: 567}};
+    var mockedRepetoireUpdateSuccessResult = {movies: [{name: 'Lion king'}, {name: 'Lion queen'}, {name: 'Alien'}, {name: 'Mars Attacks'}]};
+    var mockedAddableMoviesAfterRepertoireUpdate = {movies: [{name: 'Lion queen'}, {name: 'Alien'}, {name: 'Mars Attacks'}]};
 
     // Save mocked events tracker
-	var mockedLastUpdateRepertoirHeader = null;
-	var mockedLastUpdateRepertoirBody = null;
+    var mockedLastUpdateRepertoirHeader = null;
+    var mockedLastUpdateRepertoirBody = null;
 
-	//Mocked services
-	beforeEach(function () {
+    //Mocked services
+    beforeEach(function () {
 
-		MockedTheaterService = {
-			'query': function (params) {
-				return {
-					'$promise': {
-						then: function (success, fail) {
-							success(mockedTheaters);
-						}
-					}
-				}
-			},
-			'save': function (params) {
-				return {
-					'$promise': {
-						then: function (success, fail) {
-							if (params.name && params.numberOfSeats && params.cinema.id) {
-								success();
-								mockedSavedTheater = params;
-							} else {
-								fail();
-							}
-						}
-					}
-				}
-			}
-		};
+        MockedTheaterService = {
+            'query': function (params) {
+                return {
+                    '$promise': {
+                        then: function (success, fail) {
+                            success(mockedTheaters);
+                        }
+                    }
+                }
+            },
+            'save': function (params, params2) {
+                return {
+                    '$promise': {
+                        then: function (success, fail) {
+
+                            if (params && params2) {
+                                success();
+                                mockedSavedTheater = [params, params2]
+                            } else {
+                                fail();
+                            }
+                        }
+                    }
+                }
+            }
+        };
 
 
-		MockedMovieService = {
-			'query': function (params) {
-				return {
-					'$promise': {
-						then: function (success, fail) {
+        MockedMovieService = {
+            'query': function (params) {
+                return {
+                    '$promise': {
+                        then: function (success, fail) {
 
-							expect(params.not_in_repertoire_with_id).toBe(mockedCinema.repertoire.id)
-							success(mockedAddableMovies);
-						}
-					}
-				}
-			}
-		};
+                            expect(params.not_in_repertoire_with_id).toBe(mockedCinema.repertoire.id)
+                            success(mockedAddableMovies);
+                        }
+                    }
+                }
+            }
+        };
 
-		MockedRepertoireService = {
-			'get': function () {
-				return {
-					'$promise': {
-						then: function (success, fail) {
-							success(mockedRepertoire);
-						}
-					}
-				}
-			},
-			'update': function (header, body) {
-				return {
-					'$promise': {
-						then: function (success, fail) {
-							mockedLastUpdateRepertoirHeader = header;
-							mockedLastUpdateRepertoirBody = body;
+        MockedRepertoireService = {
+            'get': function () {
+                return {
+                    '$promise': {
+                        then: function (success, fail) {
+                            success(mockedRepertoire);
+                        }
+                    }
+                }
+            },
+            'update': function (header, body) {
+                return {
+                    '$promise': {
+                        then: function (success, fail) {
+                            mockedLastUpdateRepertoirHeader = header;
+                            mockedLastUpdateRepertoirBody = body;
                             mockedAddableMovies = mockedAddableMoviesAfterRepertoireUpdate;
-							success(mockedRepetoireUpdateSuccessResult);
-						}
-					}
-				}
-			}
-		};
+                            success(mockedRepetoireUpdateSuccessResult);
+                        }
+                    }
+                }
+            }
+        };
 
-		MockedCinemaService = {
-			'get': function () {
-				return {
-					'$promise': {
-						then: function (success, fail) {
-							success(mockedCinema);
-						}
-					}
-				}
-			}
-		};
+        MockedCinemaService = {
+            'get': function () {
+                return {
+                    '$promise': {
+                        then: function (success, fail) {
+                            success(mockedCinema);
+                        }
+                    }
+                }
+            }
+        };
 
-		Mocked$routeParams = {
-			'id': 1
-		};
+        Mocked$routeParams = {
+            'id': 1
+        };
 
-	});
+    });
 
-	//Injections, defining which mocked services to use instead of the real ones
-	beforeEach(inject(function (_$controller_) {
-		$controller = _$controller_;
-		$scope = {};
+    //Injections, defining which mocked services to use instead of the real ones
+    beforeEach(inject(function (_$controller_) {
+        $controller = _$controller_;
+        $scope = {};
+        Mocked$rootScope = {
+            "alert": function () {
+            }, "genericError": function () {
+                Mocked$rootScope.alert("Error!")
+            }, "errorHandler": function () {
+            }
+        };
+        $controller('cinemaController', {
+            $scope: $scope, $rootScope: Mocked$rootScope, $routeParams: Mocked$routeParams,
+            MovieService: MockedMovieService, TheaterService: MockedTheaterService,
+            RepertoireService: MockedRepertoireService, CinemaService: MockedCinemaService
+        });
+    }));
 
-		$controller('cinemaController', {
-			$scope: $scope, $routeParams: Mocked$routeParams,
-			MovieService: MockedMovieService, TheaterService: MockedTheaterService,
-			RepertoireService: MockedRepertoireService, CinemaService: MockedCinemaService
-		});
-	}));
+    /* Test specifications */
 
-	/* Test specifications */
+    // Repertoire tests
+    describe("Repertoire", function () {
 
-	// Repertoire tests
-	describe("Repertoire", function () {
+        it("Check getter for all movies in repertoires is working on start", function () {
+            expect($scope.getMoviesInRepertoire()).toEqual(mockedMoviesInRepertoire);
+        });
 
-		it("Check getter for all movies in repertoires is working on start", function () {
-			expect($scope.getMoviesInRepertoire()).toEqual(mockedMoviesInRepertoire);
-		});
+        it("Check getter for all movies that can be addable to repertoire is working on start", function () {
+            expect($scope.getAddableMovies()).toEqual(mockedAddableMovies);
+        });
 
-		it("Check getter for all movies that can be addable to repertoire is working on start", function () {
-			expect($scope.getAddableMovies()).toEqual(mockedAddableMovies);
-		});
+        it("Check save is called with correct params when add movie to repertoire (both movies-in-repertoire and movies-can-be-added must be refreshed after call)", function () {
+            $scope.add_movie_to_repertoire_select = {id: 123, name: "Mars Attacks"};
+            $scope.addMovieToRepertoire();
 
-		it("Check save is called with correct params when add movie to repertoire (both movies-in-repertoire and movies-can-be-added must be refreshed after call)", function () {
-			$scope.add_movie_to_repertoire_select = {id: 123, name: "Mars Attacks"};
-			$scope.addMovieToRepertoire();
+            expect(mockedLastUpdateRepertoirHeader).toEqual({"id": 567, "add_movie_with_id": 123});
+            expect(mockedLastUpdateRepertoirBody).toEqual({"id": 567});
 
-			expect(mockedLastUpdateRepertoirHeader).toEqual({"id": 567, "add_movie_with_id": 123});
-			expect(mockedLastUpdateRepertoirBody).toEqual({"id": 567});
+            expect($scope.getMoviesInRepertoire()).toEqual(mockedRepetoireUpdateSuccessResult.movies);
+            expect($scope.getAddableMovies()).toEqual(mockedAddableMoviesAfterRepertoireUpdate);
+        });
 
-			expect($scope.getMoviesInRepertoire()).toEqual(mockedRepetoireUpdateSuccessResult.movies);
-			expect($scope.getAddableMovies()).toEqual(mockedAddableMoviesAfterRepertoireUpdate);
-		});
+        it("Check remove movie from repertoire (both movies-in-repertoire and movies-can-be-added must be refreshed after call)", function () {
+            $scope.removeMovieFromRepertoire(178);
 
-		it("Check remove movie from repertoire (both movies-in-repertoire and movies-can-be-added must be refreshed after call)", function () {
-			$scope.removeMovieFromRepertoire(178);
+            expect(mockedLastUpdateRepertoirHeader).toEqual({"id": 567, "remove_movie_with_id": 178});
+            expect(mockedLastUpdateRepertoirBody).toEqual({"id": 567});
 
-			expect(mockedLastUpdateRepertoirHeader).toEqual({"id": 567, "remove_movie_with_id": 178});
-			expect(mockedLastUpdateRepertoirBody).toEqual({"id": 567});
+            expect($scope.getMoviesInRepertoire()).toEqual(mockedRepetoireUpdateSuccessResult.movies); //note/confusing: the mockedRepetoireUpdateSuccessResult is based on a list of added movies. The same list work here since we are just compareing(toEqual) and mocked service do not actual modify list in a correct way
+            expect($scope.getAddableMovies()).toEqual(mockedAddableMoviesAfterRepertoireUpdate);
+        });
 
-			expect($scope.getMoviesInRepertoire()).toEqual(mockedRepetoireUpdateSuccessResult.movies); //note/confusing: the mockedRepetoireUpdateSuccessResult is based on a list of added movies. The same list work here since we are just compareing(toEqual) and mocked service do not actual modify list in a correct way
-			expect($scope.getAddableMovies()).toEqual(mockedAddableMoviesAfterRepertoireUpdate);
-		});
+    });
 
-	});
+    // Theater tests
+    describe("Theaters", function () {
+        it("Gets all theaters", function () {
+            expect($scope.getTheaters()).toEqual(mockedTheaters);
+        });
 
-	// Theater tests
-	describe("Theaters", function () {
-		it("Gets all theaters", function () {
-			expect($scope.getTheaters()).toEqual(mockedTheaters);
-		});
+        it("Gets the id of the current cinema", function () {
+            expect($scope.getCurrentCinemaId()).toEqual(1);
+        });
 
-		it("Gets the id of the current cinema", function () {
-			expect($scope.getCurrentCinemaId()).toEqual(1);
-		});
-
-		it("Stores the theater data in an object when submit is clicked", function () {
-			$scope.add_theater_name = "sal03";
-			$scope.add_theater_seats = 100;
-
-			expect($scope.add_theater_disabled).toBeUndefined();
-
-			$scope.updateTheater();
-
-			expect($scope.newTheater.name).toBe("sal03");
-			expect($scope.newTheater.disabled).toBe(false);
-			expect($scope.newTheater.numberOfSeats).toBe(100);
-			expect($scope.newTheater.cinema.id).toEqual(1);
-		});
-
-		it("Sends the theater object to the API via POST", function () {
-			$scope.add_theater_name = "sal04";
-			$scope.add_theater_seats = 100;
-			$scope.add_theater_disabled = false;
-			$scope.updateTheater();
-
-			expect(mockedSavedTheater).toEqual({cinema: {id:1}, name: "sal04", disabled: false, numberOfSeats: 100});
-		});
+        it("Stores the theater data in an object when submit is clicked", function () {
+            $scope.theater_name = "sal03";
+            $scope.number_of_rows = 12;
+            $scope.number_of_seats = 12;
 
 
-		it("Logs a success message if the add theater-API call is successful", function () {
-			spyOn($scope, 'alert');
+            $scope.addNewTheater();
 
-			$scope.add_theater_name = "sal04";
-			$scope.add_theater_seats = 100;
-			$scope.add_theater_disabled = false;
-			$scope.updateTheater();
+            expect($scope.newTheater.name).toBe("sal03");
+            expect($scope.newTheater.cinema.id).toEqual(1);
+        });
 
-			expect($scope.alert).toHaveBeenCalledWith("Success!");
-		});
+        it("Sends the theater object to the API via POST", function () {
+            $scope.theater_name = "sal03";
+            $scope.number_of_rows = 12;
+            $scope.number_of_seats = 12;
+            $scope.addNewTheater();
 
-		it("Logs an error message if the add theater-API call is unsuccessful", function () {
-			spyOn($scope, 'alert');
-			$scope.updateTheater();
+            expect(mockedSavedTheater).toEqual([Object({number_of_rows: 12, max_row_size: 12}), Object({
+                name: 'sal03',
+                cinema: Object({id: 1})
+            })]);
+        });
 
-			expect($scope.alert).toHaveBeenCalledWith("Error!");
 
-		});
+        it("Logs a success message if the add theater-API call is successful", function () {
+            spyOn(Mocked$rootScope, 'alert');
 
-		it("Resets the input fields after add theater", function () {
-			$scope.resetFields();
+            $scope.theater_name = "sal03";
+            $scope.number_of_rows = 12;
+            $scope.number_of_seats = 12;
+            $scope.addNewTheater();
 
-			expect($scope.add_theater_name).toEqual("");
-			expect($scope.add_theater_disabled).toEqual(false);
-			expect($scope.add_theater_seats).toEqual(0);
+            expect(Mocked$rootScope.alert).toHaveBeenCalledWith('Success! ', 'Theater sal03 was created', 1);
+        });
 
-		})
-	});
+        it("Logs an error message if the add theater-API call is unsuccessful", function () {
+            spyOn(Mocked$rootScope, 'alert');
+            $scope.addNewTheater();
 
-	//Cinema test
-	describe("Cinema", function () {
-		it("Prints the cinema name", function() {
-			expect($scope.cinema_name).toEqual($scope.getCurrentCinema().name);
-		});
+            expect(Mocked$rootScope.alert).toHaveBeenCalledWith('Error!');
+        });
 
-		it("Disables the submit button if cinema name is empty", function () {
+        it("Resets the input fields after add theater", function () {
+            $scope.resetFields();
+
+            expect($scope.add_theater_name).toEqual("");
+            expect($scope.add_theater_disabled).toEqual(false);
+            expect($scope.add_theater_seats).toEqual(0);
+
+        })
+    });
+
+    //Cinema test
+    describe("Cinema", function () {
+        it("Prints the cinema name", function () {
+            expect($scope.cinema_name).toEqual($scope.getCurrentCinema().name);
+        });
+
+        it("Disables the submit button if cinema name is empty", function () {
             $scope.edited_cinema_name = "";
             expect($scope.validateCinemaNameForm()).toEqual(false);
             $scope.edited_cinema_name = "Wololo";
-			expect($scope.validateCinemaNameForm()).toEqual(true);
-		});
-	});
+            expect($scope.validateCinemaNameForm()).toEqual(true);
+        });
+    });
 });
