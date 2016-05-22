06cc5708575c20048d775f0bfe358d57ee41bccd
Erik Wiberg <erik.wiberg.87@gmail.com>
2016-05-22 13:33:10 +0200

    add exception intercept for non-custom datetime format exception

--- src/main/java/filmr/controllers/BaseController.java
+++ src/main/java/filmr/controllers/BaseController.java
@@ -1,14 +1,18 @@
 package filmr.controllers;
 
+import java.time.format.DateTimeParseException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import filmr.helpers.exceptions.FilmrBaseException;
 import filmr.helpers.exceptions.FilmrExceptionModel;
+import filmr.helpers.exceptions.FilmrInvalidDateFormatException;
 
 /**
  * 
@@ -27,6 +31,26 @@ public class BaseController {
     	logger.debug("Catching custom error in method inherrited from BaseController.java ");
     	FilmrExceptionModel exceptionModel = new FilmrExceptionModel(req, ex);
         return new ResponseEntity<FilmrExceptionModel>(exceptionModel, ex.getHttpStatus());
+    }
+    
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseBody
+    public ResponseEntity<FilmrExceptionModel> handleBadDateTimeFormat(HttpServletRequest req, MethodArgumentTypeMismatchException ex) {
+    	
+    	logger.debug("caught non-custom exception: " + ex);
+    	logger.debug("caught non-custom exception with message : " + ex.getLocalizedMessage());
+    	
+    	if(ex.getLocalizedMessage().contains("java.time.format.DateTimeParseException")) {
+    		// something wrong with the date format
+    		logger.debug("caught probable datetime format exception: " + ex);
+    		FilmrInvalidDateFormatException datetimeException = new FilmrInvalidDateFormatException();
+    		FilmrExceptionModel exceptionModel = new FilmrExceptionModel(req, datetimeException);
+    		return new ResponseEntity<FilmrExceptionModel>(exceptionModel, datetimeException.getHttpStatus());    		
+    	}
+    	
+    	// else just throw the error and let Spring handle it
+    	throw ex;
+    	
     }
 
 }
